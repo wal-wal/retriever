25541683249f8884e98f7f1de06698e82a739f85
yoochanhong <chanhongy6@naver.com>
2023-12-16 23:59:25 +0900

    feat :: DeleteMantra 기능 구현

--- presentation/presentation/mantra_port_impl.go
+++ presentation/presentation/mantra_port_impl.go
@@ -4,18 +4,22 @@ import (
 	"application/domain/mantra/dto/request"
 	"application/domain/mantra/dto/response"
 	"application/domain/mantra/use_case"
+	"github.com/google/uuid"
 )
 
 type MantraPortImpl struct {
 	readAllMantrasUseCase use_case.ReadAllMantrasUseCase
 	createMantraUseCase   use_case.CreateMantraUseCase
+	deleteMantraUseCase   use_case.DeleteMantraUseCase
 }
 
-func New(ReadAllMantrasUseCase use_case.ReadAllMantrasUseCase,
-	CreateMantraUseCase use_case.CreateMantraUseCase) *MantraPortImpl {
+func New(readAllMantrasUseCase use_case.ReadAllMantrasUseCase,
+	createMantraUseCase use_case.CreateMantraUseCase,
+	deleteMantraUseCase use_case.DeleteMantraUseCase) *MantraPortImpl {
 	return &MantraPortImpl{
-		readAllMantrasUseCase: ReadAllMantrasUseCase,
-		createMantraUseCase:   CreateMantraUseCase,
+		readAllMantrasUseCase: readAllMantrasUseCase,
+		createMantraUseCase:   createMantraUseCase,
+		deleteMantraUseCase:   deleteMantraUseCase,
 	}
 }
 func (r *MantraPortImpl) ReadAllMantras() []response.ReadAllMantraResDTO {
@@ -24,4 +28,8 @@ func (r *MantraPortImpl) ReadAllMantras() []response.ReadAllMantraResDTO {
 
 func (r *MantraPortImpl) CreateMantra(dto request.CreateMantraReqDTO) error {
 	return r.createMantraUseCase.Execute(dto)
+}
+
+func (r *MantraPortImpl) DeleteMantra(id uuid.UUID) error {
+	return r.deleteMantraUseCase.Execute(id)
 }
