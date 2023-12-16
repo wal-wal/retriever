64aa09b20537f71a1afac6317f28b7eabedfd66b
yoochanhong <chanhongy6@naver.com>
2023-12-16 23:57:02 +0900

    feat :: DeleteMantra 메소드 정의

--- application/port/primary_port/mantra_port.go
+++ application/port/primary_port/mantra_port.go
@@ -3,9 +3,11 @@ package primary_port
 import (
 	"application/domain/mantra/dto/request"
 	"application/domain/mantra/dto/response"
+	"github.com/google/uuid"
 )
 
 type MantraPort interface {
 	ReadAllMantras() []response.ReadAllMantraResDTO
 	CreateMantra(dto request.CreateMantraReqDTO) error
+	DeleteMantra(id uuid.UUID) error
 }
--- application/port/secondary_port/mantra_repository.go
+++ application/port/secondary_port/mantra_repository.go
@@ -1,8 +1,12 @@
 package secondary_port
 
-import "application/domain/mantra/entity"
+import (
+	"application/domain/mantra/entity"
+	"github.com/google/uuid"
+)
 
 type MantraRepository interface {
 	ReadAllMantras() []entity.Mantra
 	CreateMantra(mantra entity.Mantra) error
+	DeleteMantra(id uuid.UUID) error
 }
