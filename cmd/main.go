e3af4df38700e299f8ec500c659a46f607d03c65
yoochanhong <chanhongy6@naver.com>
2023-12-16 21:30:36 +0900

    feat :: mock 데이터 시간 추가

--- cmd/main.go
+++ cmd/main.go
@@ -6,16 +6,17 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"persistence/persistence"
 	"presentation/presentation"
+	"time"
 )
 
 func main() {
 	app := fiber.New()
-
+	t := time.Now()
 	m := make(map[int]entity.Mantra)
-	m[0] = entity.Mantra{Id: 1, Writer: "김철우", Content: "내 코드는 레거시 코드다"}
-	m[1] = entity.Mantra{Id: 2, Writer: "정지관", Content: "내가 짱이다. 남에게 주눅들 이유 따위는 없다"}
-	m[2] = entity.Mantra{Id: 3, Writer: "이정호", Content: "푸른 언덕에~~"}
-	m[3] = entity.Mantra{Id: 4, Writer: "손영달", Content: "엄마한테서 최대한 3억을 뜯어내라"}
+	m[0] = entity.Mantra{Id: 1, Writer: "김철우", Content: "내 코드는 레거시 코드다", CreatedAt: t.Add(time.Minute)}
+	m[1] = entity.Mantra{Id: 2, Writer: "정지관", Content: "내가 짱이다. 남에게 주눅들 이유 따위는 없다", CreatedAt: t.Add(time.Minute * 2)}
+	m[2] = entity.Mantra{Id: 3, Writer: "이정호", Content: "푸른 언덕에~~", CreatedAt: t.Add(time.Minute * 3)}
+	m[3] = entity.Mantra{Id: 4, Writer: "손영달", Content: "엄마한테서 최대한 3억을 뜯어내라", CreatedAt: t.Add(time.Minute * 4)}
 	mantraRepository := persistence.New(m)
 	readAllMantraUseCase := use_case.New(mantraRepository)
 	mantraController := presentation.New(*readAllMantraUseCase)
