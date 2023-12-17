7897beef0f8493539904c925b9557e874e5ed1a2
yoochanhong <chanhongy6@naver.com>
2023-12-17 16:49:38 +0900

    feat :: ToEntity 삭제

--- application/domain/mantra/dto/request/create_mantra_req_dto.go
+++ application/domain/mantra/dto/request/create_mantra_req_dto.go
@@ -1,21 +1,6 @@
 package request
 
-import (
-	"application/domain/mantra/entity"
-	"github.com/google/uuid"
-	"time"
-)
-
 type CreateMantraReqDTO struct {
 	Content string `json:"content"`
 	Writer  string `json:"writer"`
-}
-
-func (c CreateMantraReqDTO) ToEntity(Id uuid.UUID, CreatedAt time.Time) entity.Mantra {
-	return entity.Mantra{
-		Id:        Id,
-		Content:   c.Content,
-		Writer:    c.Writer,
-		CreatedAt: CreatedAt,
-	}
 }
