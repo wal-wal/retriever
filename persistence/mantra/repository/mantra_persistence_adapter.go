4142cfe2ffc2810a1a475f92037309e38346b46f
yoochanhong <chanhongy6@naver.com>
2024-04-23 10:18:37 +0900

    feat :: persistence adapter 구현

--- persistence/mantra/repository/mantra_persistence_adapter.go
+++ persistence/mantra/repository/mantra_persistence_adapter.go
@@ -1,33 +1,44 @@
 package repository
 
 import (
-	"application/domain/mantra/entity"
+	"application/domain/mantra/model"
+	"database/sql"
 	"github.com/google/uuid"
+	entity2 "persistence/mantra/entity"
+	"persistence/mantra/mapper"
 )
 
 type MantraPersistenceAdapter struct {
-	m map[uuid.UUID]entity.Mantra
+	db     *sql.DB
+	mapper mapper.MantraMapper
 }
 
-func NewMantraPersistenceAdapter(m map[uuid.UUID]entity.Mantra) *MantraPersistenceAdapter {
+func NewMantraPersistenceAdapter(db *sql.DB) *MantraPersistenceAdapter {
 	return &MantraPersistenceAdapter{
-		m: m,
+		db: db,
 	}
 }
 
-func (r *MantraPersistenceAdapter) ReadAllMantras() (list []entity.Mantra, err error) {
-	for _, v := range r.m {
-		list = append(list, v)
+func (r *MantraPersistenceAdapter) ReadAllMantras() (list []model.Mantra, err error) {
+	rows, err := r.db.Query("SELECT * FROM tbl_mantra")
+	if err != nil {
+		return list, err
+	}
+	mantraEntity := entity2.MantraEntity{}
+	for rows.Next() {
+		err = rows.Scan(&mantraEntity.MantraId, &mantraEntity.Writer, &mantraEntity.Content, &mantraEntity.CreatedAt)
+		list = append(list, *r.mapper.ToDomain(mantraEntity))
 	}
 	return list, nil
 }
 
-func (r *MantraPersistenceAdapter) CreateMantra(mantra entity.Mantra) error {
-	r.m[mantra.Id] = mantra
-	return nil
+func (r *MantraPersistenceAdapter) CreateMantra(Mantra model.Mantra) error {
+	mantraEntity := r.mapper.ToEntity(Mantra)
+	_, err := r.db.Exec("insert into tbl_mantra values (?, ?, ?, ?)", mantraEntity.MantraId, mantraEntity.Writer, mantraEntity.Content, mantraEntity.CreatedAt)
+	return err
 }
 
-func (r *MantraPersistenceAdapter) DeleteMantra(id uuid.UUID) error {
-	delete(r.m, id)
-	return nil
+func (r *MantraPersistenceAdapter) DeleteMantra(MantraId uuid.UUID) error {
+	_, err := r.db.Exec("delete from tbl_mantra where mantraId = ?", MantraId.String())
+	return err
 }
