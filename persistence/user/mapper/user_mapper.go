8edcb00fac1883e0a7ff72ca6010b3cfe9f654ab
yoochanhong <chanhongy6@naver.com>
2024-05-04 21:41:58 +0900

    refactor :: public method로 변경

--- persistence/user/mapper/user_mapper.go
+++ persistence/user/mapper/user_mapper.go
@@ -8,16 +8,16 @@ import (
 type UserMapper struct {
 }
 
-func (r *UserMapper) toDomain(entity entity.UserEntity) *model.User {
-	return &model.User{
+func (r *UserMapper) ToDomain(entity entity.UserEntity) model.User {
+	return model.User{
 		UserId:   entity.UserId,
 		Password: entity.Password,
 		Name:     entity.Name,
 	}
 }
 
-func (r *UserMapper) toEntity(model model.User) *entity.UserEntity {
-	return &entity.UserEntity{
+func (r *UserMapper) ToEntity(model model.User) entity.UserEntity {
+	return entity.UserEntity{
 		UserId:   model.UserId,
 		Password: model.Password,
 		Name:     model.Name,
