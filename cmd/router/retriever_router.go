6efa661d829f2d282642dd213c145f047f8e75fc
yoochanhong <chanhongy6@naver.com>
2024-05-05 12:12:18 +0900

    feat :: 한곳에서 모두 라우팅하도록 변경

---
+++ cmd/router/retriever_router.go
@@ -0,0 +1,38 @@
+package router
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"retriever-presentation/auth"
+	"retriever-presentation/mantra"
+	"retriever-presentation/user"
+)
+
+type RetrieverRouter struct {
+	mantra mantra_persentation.MantraWebAdapter
+	user   user_presentation.UserWebAdapter
+	auth   auth_presentation.AuthWebAdapter
+}
+
+func NewRouter(mantra mantra_persentation.MantraWebAdapter,
+	user user_presentation.UserWebAdapter,
+	auth auth_presentation.AuthWebAdapter) *RetrieverRouter {
+	return &RetrieverRouter{
+		mantra: mantra,
+		user:   user,
+		auth:   auth,
+	}
+}
+
+func (r *RetrieverRouter) Initialize(app *fiber.App) {
+	userRouter := app.Group("/user")
+	mantraRouter := app.Group("/mantra")
+	authRouter := app.Group("/auth")
+
+	userRouter.Post("/sign-up", r.user.CreateUser)
+
+	mantraRouter.Get("/", r.mantra.ReadAllMantras)
+	mantraRouter.Post("/", r.mantra.CreateMantra)
+	mantraRouter.Delete("/:id", r.mantra.DeleteMantra)
+
+	authRouter.Post("/sign-in", r.auth.SignIn)
+}
