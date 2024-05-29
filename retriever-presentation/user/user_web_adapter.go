c0f97f7ec0d102c7ef85d67578a242b30a93af9c
yoochanhong <chanhongy6@naver.com>
2024-05-29 10:50:33 +0900

    feat :: user 도메인 유효성 검사

--- retriever-presentation/user/user_web_adapter.go
+++ retriever-presentation/user/user_web_adapter.go
@@ -1,6 +1,7 @@
 package user_presentation
 
 import (
+	"errors"
 	"github.com/gofiber/fiber/v2"
 	"retriever-core/user/dto/request"
 	"retriever-core/user/primary"
@@ -19,9 +20,24 @@ func NewUserWebAdapter(userPort user_primary.UserPrimaryPort) *UserWebAdapter {
 func (r *UserWebAdapter) CreateUser(ctx *fiber.Ctx) error {
 	dto := new(user_request.CreateUserReqDTO)
 	_ = ctx.BodyParser(dto)
-	err := r.userPort.CreateUser(*dto)
-	if err != nil {
+	if err := createUserReqDTOValidator(*dto); err != nil {
+		return ctx.Status(500).JSON(map[string]string{"message": err.Error()})
+	}
+	if err := r.userPort.CreateUser(*dto); err != nil {
 		return ctx.Status(409).JSON(map[string]string{"message": err.Error()})
 	}
 	return ctx.SendStatus(201)
+}
+
+func createUserReqDTOValidator(dto user_request.CreateUserReqDTO) error {
+	if dto.UserId == "" {
+		return errors.New("아이디를 입력해주세요")
+	}
+	if dto.Password == "" {
+		return errors.New("비밀번호를 입력해주세요")
+	}
+	if dto.Name == "" {
+		return errors.New("이름을 입랙해주세요")
+	}
+	return nil
 }
